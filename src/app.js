ceba82b2fcab06d3978e477437e99f1915d42b4a
Camila <camilarodriguescrs13@gmail.com>
2023-08-11 22:56:08 -0300

    rota get test

--- src/app.js
+++ src/app.js
@@ -5,14 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const usuarios = [{
-    username: "Fulaninha", 
-    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIS998lvY2EbqPDkBSY6JrDdctsdfmuhxFAvjO7-M&s"
-},
-{
-    username: "Siclano", 
-    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9bPppzDXokIGZH5SNFz1YEIx0DtQK0GVC8X4fc67mX9Dovsze2_1irNxQUcyVA7hcfvc&usqp=CAU" 
-}];
+const usuarios = [];
 const tweets = [];
 
 app.post('/sign-up', (req, res) => {
@@ -43,15 +36,16 @@ app.post('/tweets', (req, res) => {
 });
 
 app.get('/tweets', (req, res) => {
-
-    const tweet = [
+    const {avatar} = req.params;
+    const tweetsList = [ ...tweets, 
         {
             username: req.body.username,
-            avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+            avatar: avatar,
             tweet: req.body.tweet
         }
     ]
-   res.send(tweet);
+   res.send(tweetsList);
 } );
+
 
 app.listen(5000, () => console.log("Running server on port 5000"));
