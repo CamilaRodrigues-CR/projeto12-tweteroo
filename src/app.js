655e253c813a42a8049882dc8181f04eaac39153
Camila <camilarodriguescrs13@gmail.com>
2023-08-11 22:19:54 -0300

    rotas post implementadas

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ import cors from "cors"
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const usuarios = []
+const usuarios = [{
+    username: "Fulaninha", 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIS998lvY2EbqPDkBSY6JrDdctsdfmuhxFAvjO7-M&s"
+},
+{
+    username: "Siclano", 
+    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9bPppzDXokIGZH5SNFz1YEIx0DtQK0GVC8X4fc67mX9Dovsze2_1irNxQUcyVA7hcfvc&usqp=CAU" 
+}];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
     const usuario = {
@@ -15,10 +23,35 @@ app.post('/sign-up', (req, res) => {
     usuarios.push(usuario);
     res.send('OK');
 
-})
+});
+
+app.post('/tweets', (req, res) => {
+    const {username} = req.body;
+    const user = usuarios.some(usuario => usuario.username === username)
+    if (user){
+        const tweet = {
+            username: req.body.username,
+            tweet: req.body.tweet
+        }
+        tweets.push(tweet)
+        res.send('OK')
+    } else {
+        res.send('UNAUTHORIZED')
+    }
+
+   
+});
 
 app.get('/tweets', (req, res) => {
-    res.send('deu certo!');
+
+    const tweet = [
+        {
+            username: req.body.username,
+            avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+            tweet: req.body.tweet
+        }
+    ]
+   res.send(tweet);
 } );
 
 app.listen(5000, () => console.log("Running server on port 5000"));
